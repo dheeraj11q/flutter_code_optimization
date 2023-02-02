f88c8d7731e2b981223c2545a7fc547ff83e4703
dheerajdigi <dheerajdigi@gmail.com>
2023-02-02 23:29:14 +0530

    file handler

---
+++ lib/web/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WebHome extends StatefulWidget {
+  const WebHome({Key? key}) : super(key: key);
+
+  @override
+  State<WebHome> createState() => _WebHomeState();
+}
+
+class _WebHomeState extends State<WebHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Web")),
+    );
+  }
+}
